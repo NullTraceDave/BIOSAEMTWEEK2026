1e43aca3f98978ac7ec163d1dda3fc5a2482e37e
Perfection <142207135+NullTraceDave@users.noreply.github.com>
2026-04-01 14:48:07 +0100

    Add files via upload

---
+++ nominee.js
@@ -0,0 +1,10 @@
+const voteData = {
+    "Rave King": [
+        { "name": "Nominee A", "votes": 0 },
+        { "name": "Nominee B", "votes": 0 }
+    ],
+    "Rave Queen": [
+        { "name": "Nominee X", "votes": 0 },
+        { "name": "Nominee Y", "votes": 0 }
+    ]
+};
