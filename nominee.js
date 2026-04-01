5ffb8d5bf0a91b5166ddd9e46a5d77a8a31d3736
Perfection <142207135+NullTraceDave@users.noreply.github.com>
2026-04-01 14:54:08 +0100

    Update nominee.js

--- nominee.js
+++ nominee.js
@@ -1,10 +1,10 @@
 const voteData = {
     "Rave King": [
-        { "name": "Nominee A", "votes": 0 },
-        { "name": "Nominee B", "votes": 0 }
+        { "name": "No nominee yet, Stay tuned", "votes": 0 },
+        
     ],
     "Rave Queen": [
-        { "name": "Nominee X", "votes": 0 },
-        { "name": "Nominee Y", "votes": 0 }
+        { "name": "No nominee yet, Stay tuned", "votes": 0 },
+        
     ]
 };
